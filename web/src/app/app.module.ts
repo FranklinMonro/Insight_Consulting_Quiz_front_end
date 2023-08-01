888e8cee3aeb98fd87b6cbcfa14cd3ee3f5429bd
FrancoGroenewald <franco@softwareworx.co.za>
2023-08-01 15:52:09 +0000

    enviroments

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -2,20 +2,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuizComponent } from './quiz/quiz.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuizComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- web/src/app/quiz/quiz-routing.module.ts
+++ web/src/app/quiz/quiz-routing.module.ts
@@ -1,8 +1,11 @@
 /* eslint-disable import/prefer-default-export */
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QuizComponent } from './quiz.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: QuizComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
