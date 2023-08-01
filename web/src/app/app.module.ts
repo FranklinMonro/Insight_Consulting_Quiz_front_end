ebc450d6ac31def1dba8bb0cea0e584ea942198a
FrancoGroenewald <franco@softwareworx.co.za>
2023-08-01 15:37:44 +0000

    quiz component

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,20 +1,23 @@
+/* eslint-disable import/prefer-default-export */
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { QuizComponent } from './quiz/quiz.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuizComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
